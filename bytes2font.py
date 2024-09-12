3dffcfb7679342474a6e1f867a4f7109730abddd
GiacomoNVS <68269880+theHEXstyle@users.noreply.github.com>
2024-09-12 11:07:40 +0200

    Update bytes2font.py

--- bytes2font.py
+++ bytes2font.py
@@ -7,10 +7,11 @@
 
 # input:  a .cpp font file
 # output: an image rendition of the font (not an actual font)
-HEX_PER_LINE = 3
+
+
+HEX_PER_LINE = 3  # <------------------------- change how many hexes constitute a font line (usually 3)
 
 from PIL import Image
-import matplotlib.pyplot as plt
 import numpy as np
 import re
 
@@ -18,9 +19,7 @@ filename = './input/FontReg36.cpp'  #<----------- change file name!
 
 font_image = []
 
-def divide_chunks(l, n):
-
-    # looping till length l
+def divide_chunks(l, n):  # this takes HEX_PER_LINE elements from a list of hexes
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
@@ -30,11 +29,10 @@ with open(filename) as file:
     matches = re.findall(r'0x([0-9abcdefABCDEF]+)', lines)  # regex to find hex values
 
 
-counter = 0
 char_lines = divide_chunks(matches, HEX_PER_LINE)
 for char_line in char_lines:
-    font_line = ''.join([bin(int(x, 16))[2:].zfill(8) for x in char_line])
-    font_line = [(int(x)-1)*-255 for x in font_line]
+    font_line = ''.join([bin(int(x, 16))[2:].zfill(8) for x in char_line])  # all hex values are transformed in 0 and 1 values and then concatenated in a string
+    font_line = [(int(x)-1)*-255 for x in font_line]  # convert the list in a 0-255 range
     if font_image == []:
         font_image = [font_line]
     else:
@@ -44,8 +42,4 @@ image = Image.fromarray(np.array(font_image, dtype=np.uint8))
 
 # Save the image
 image.save(filename.replace('/input/', '/output/').replace('.cpp', '.png'))
-
-
-
-
 
